b01086a7b0b73b2cf426c0ef6ad3b96c39bfeb60
KeiGarcia <keilataniagarcia3@gmail.com>
2022-08-05 19:52:06 -0300

    consumir api

---
+++ main.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+
+const URL =  "https://apipetshop.herokuapp.com/api/articulos"
+
+const { createApp } = Vue
+createApp({
+
+    data(){
+        return{
+            data: [],
+            juguetes: [],
+            farmacia: []
+        }
+    },
+
+    created(){
+        fetch(URL)
+        .then(res => res.json())
+        .then(datos => {
+            this.data = datos.response
+            this.soloJuguetes()
+            this.soloMedicamentos()
+        })
+        .catch(err => console.log(err))
+    },
+
+    methods: {
+        soloJuguetes : function(){
+            return this.juguetes = this.data.filter(elemento => elemento.tipo.includes("Juguete"))
+        },
+        
+        soloMedicamentos : function(){
+            return this.farmacia = this.data.filter(elemento => elemento.tipo.includes("Medicamento"))
+        } 
+    },
+
+    computed: {
+        
+    }
+}).mount('#container')

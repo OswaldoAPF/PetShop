e0f2da891531140050dac3f3f9a5a42b315df109
LiliTaschini <liliantaschini@gmail.com>
2022-08-10 10:52:41 -0300

    actualización js y botones redes sociales

--- main.js
+++ main.js
@@ -42,3 +42,46 @@ createApp({
     }
 }).mount('#container')
 
+
+//jQuery time
+let next = document.querySelector(".next"); // Boton Siguiente
+let prev = document.querySelector(".prev"); // Boton Atras
+let progressIcon = document.querySelector("#progressbar li"); // Barra de Progreso
+let fieldset = document.querySelector(".fieldset");
+let fieldsetDos = document.querySelector(".fieldsetDos"); // Cartas del Form
+let nombre = document.querySelector(".nombreRequerido");
+let apellido = document.querySelector(".apellidoRequerido");
+let alert = document.querySelector(".alert");
+let email = document.querySelector(".mailRequerido");
+let otro = document.querySelector(".otro");
+let campoOtro = document.querySelector(".campoOtro");
+next.addEventListener("click", function (e) {
+  if (email.value && nombre.value && apellido.value) {
+    fieldsetDos.classList.toggle("siguiente");
+    fieldset.classList.toggle("siguiente");
+  } else {
+    alert.innerHTML = "";
+    alert.innerHTML = `<p> *Complete los campos requeridos por favor </p>`;
+  }
+});
+
+prev.addEventListener("click", function (e) {
+  alert.innerHTML = "";
+  fieldsetDos.classList.toggle("siguiente");
+  fieldset.classList.toggle("siguiente");
+});
+
+otro.addEventListener("click", function (e) {
+  if (otro.checked) {
+    campoOtro.innerHTML = `<input type="text" name="otro" placeholder="¿Cuál?"></input>`;
+  }
+});
+
+Swal.fire({
+  position: 'top-end',
+  icon: 'success',
+  title: 'Your work has been saved',
+  showConfirmButton: false,
+  timer: 1500
+})
+
